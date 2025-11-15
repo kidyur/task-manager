16e6dbf6f20881e2f0a0c4a6f38d74c2d80f0acd
arisond <ruzmanov44gd@mail.ru>
2025-11-15 23:43:42 +0300

    tags

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -2,27 +2,22 @@ const taskContainer = document.getElementById("tasks-page__task-list");
 const addTaskButton = document.getElementById("tasks-page__add-task-button");
 const taskNameInput = document.getElementById("tasks-page__task-name-input");
 const dateInput = document.getElementById("tasks-page__date-input")
+const tagLine = document.getElementById("tasks-page__tag-line")
+const tagInput = document.getElementById("tasks-page__tag-input")
+const addTagButton = document.getElementById("tasks-page__add-tag-button")
+const filterButton = document.getElementById("tasks-page__filter-button")
 
-
-let tasks = [];
 let dates = {};
 
-dateInput.value = new Date().toISOString().split('T')[0];
+let tags = [];
 
-
-addTaskButton.addEventListener('click', () => {
-    createTask(taskNameInput.value, dateInput.value);    
-    taskNameInput.value = '';
-});
-
-const today = new Date().toISOString().split('T')[0];
-                 
 
 class Task {
-    constructor(name, date, element) {
+    constructor(name, date, element, tags) {
         this.name = name;
         this.date = date;     
         this.element = element;          
+        this.tags = tags;
     }    
 
     remove() {
@@ -52,6 +47,47 @@ class TaskDate {
     }
 }
 
+class Tag {
+    constructor(name, el) {
+        this.selected = false;
+        this.name = name;     
+        this.el = el;   
+    }
+
+    setSelected(val) {
+        if (val != this.selected) {            
+            if (val) {            
+                this.el.className = 'tasks-page__tag--enabled';
+            }
+            else {
+                this.el.className = 'tasks-page__tag';
+            }
+        }
+        this.selected = val;
+    }
+}
+
+
+start();
+
+function start() {      
+    createTag('вычмаш');
+
+    dateInput.value = new Date().toISOString().split('T')[0];
+    
+
+    addTagButton.addEventListener('click', () => {
+        createTag(tagInput.value);
+        tagInput.value = '';    
+    });    
+    addTaskButton.addEventListener('click', () => {
+        createTask(taskNameInput.value, dateInput.value);    
+        taskNameInput.value = '';
+    });
+
+    const today = new Date().toISOString().split('T')[0];
+}
+                 
 
 function createTask(name, dateStr) {        
     let date = null;
@@ -71,11 +107,19 @@ function createTask(name, dateStr) {
     element.className = 'tasks-page__task';
     element.innerHTML = `
         <div class="tasks-page__task-name"></div>
+        <div class="tasks-page__task-tags"></div>
         <button class="tasks-page__complete-task-btn"></button>
     `; 
     
     element.getElementsByClassName('tasks-page__complete-task-btn')[0].addEventListener('click', () => newTask.remove());
     element.getElementsByClassName('tasks-page__task-name')[0].textContent = name;
+
+    taskTagsEl = element.getElementsByClassName('tasks-page__task-tags')[0];
+    for (let t of tags) {
+        if (t.selected) {
+            taskTagsEl.innerHTML += `#${t.name}\n`;
+        }
+    }
     
     let nextElement = null;    
     for (let i of Array.from(taskContainer.children)) {
@@ -94,6 +138,8 @@ function createTask(name, dateStr) {
     else {
         taskContainer.appendChild(element);    
     }    
+
+    deselectAllTags();
 }
 
 function createSeparator(dateStr) {
@@ -123,4 +169,31 @@ function createSeparator(dateStr) {
         taskContainer.appendChild(dateElement);
     }
     return dateElement;
+}
+
+
+function createTag(name) {   
+    let tagEl = document.createElement('button');
+    tagEl.className = 'tasks-page__tag';
+    tagEl.innerHTML = '#' + name;    
+
+    let tag = new Tag(name, tagEl);
+    
+    tagEl.addEventListener('click', () => {
+        tag.setSelected(!tag.selected);
+    });    
+
+    tags.push(tag);
+    
+    tagLine.appendChild(tagEl);
+}
+
+function deselectAllTags() {
+    for (let t of tags) {
+        t.setSelected(false);   
+    }
+}
+
+function filter() {    
+    
 }
